2b0421d18291ef01cfc3afa8041ad677b8272c08
fpc <izgifth@gmail.com>
2020-02-13 14:38:03 -0500

    Initial commit

---
+++ venv/app.py
@@ -0,0 +1,214 @@
+""" Implement a class for fractions that supports
+    addition, subtraction, multiplication, division and check if
+    equal, not equal, less than, less than or equal to,
+    greater thangreater than or equal to
+
+    author: Fatih IZGI
+    date: 09-Feb-2020
+    version: python 3.6.9
+"""
+
+class Fraction:
+    """ Support addition, subtraction, multiplication, division
+        and checking the equation of fractions
+        with a simple algorithm
+    """
+
+    def __init__(self, num: float, denom: float) -> None:
+        """ store num and denom
+            Raise ZeroDivisionError on 0 denominator
+        """
+
+        if denom == 0.0:
+            raise ZeroDivisionError
+
+        self.num: float = num
+        self.denom: float = denom
+
+    def __str__(self) -> str:
+        """ return a String to display fractions """
+        return f"{self.num}/{self.denom}"
+
+    def __add__(self, other: "Fraction") -> "Fraction":
+        """ Add two fractions using simplest approach.
+            Calculate new numerator and denominator and return new Fraction
+        """
+        new_self_num: float = self.num * other.denom
+        new_other_num: float = other.num * self.denom
+
+        final_num: float = new_self_num + new_other_num
+        final_denom: float = self.denom * other.denom
+
+        final_fraction: Fraction = Fraction(final_num, final_denom)
+
+        return final_fraction
+
+    def __sub__(self, other: "Fraction") -> "Fraction":
+        """ subtract two fractions using simplest approach
+            Calculate new numerator and denominator and return new Fraction
+        """
+        new_self_num: float = self.num * other.denom
+        new_other_num: float = other.num * self.denom
+
+        final_num: float = new_self_num - new_other_num
+        final_denom: float = self.denom * other.denom
+
+        final_fraction: Fraction = Fraction(final_num, final_denom)
+
+        return final_fraction
+
+    def __mul__(self, other: "Fraction") -> "Fraction":
+        """ Multiply two fractions using simplest approach
+            Calculate new numerator and denominator and return new Fraction
+        """
+        final_num: float = self.num * other.num
+        final_denom: float = self.denom * other.denom
+
+        final_fraction: Fraction = Fraction(final_num, final_denom)
+
+        return final_fraction
+
+    def __truediv__(self, other: "Fraction") -> "Fraction":
+        """ Add two fractions using simplest approach.
+            Calculate new numerator and denominator and return new Fraction
+        """
+        final_num: float = self.num * other.denom
+        final_denom: float = self.denom * other.num
+
+        final_fraction: Fraction = Fraction(final_num, final_denom)
+
+        return final_fraction
+
+    def __eq__(self, other: "Fraction") -> bool:
+        """ return True/False if the two fractions are equivalent """
+        if self.num * other.denom == self.denom * other.num:
+            return True
+        else:
+            return False # the fractions are not equal
+
+    def __ne__(self, other: "Fraction") -> bool:
+        """ return True/False if the two fractions are not equivalent """
+        if self.num * other.denom == self.denom * other.num:
+            return False
+        else:
+            return True # the fractions are not equal
+
+    def __lt__(self, other: "Fraction") -> bool:
+        """ return True/False if the first fraction is less than the second fraction """
+        f1: float = self.num / self.denom
+        f2: float = other.num / other.denom
+
+        if f1 < f2:
+            return True
+        else:
+            return False
+
+    def __le__(self, other: "Fraction") -> bool:
+        """ return True/False if the first fraction is less than or equal to the second fraction """
+        f1: float = self.num / self.denom
+        f2: float = other.num / other.denom
+
+        if f1 <= f2:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other: "Fraction") -> bool:
+        """ return True/False if the first fraction is greater than the second fraction """
+        f1: float = self.num / self.denom
+        f2: float = other.num / other.denom
+
+        if f1 > f2:
+            return True
+        else:
+            return False
+
+    def __ge__(self, other: "Fraction") -> bool:
+        """ return True/False if the first fraction is greater than or equal to the second fraction """
+        f1: float = self.num / self.denom
+        f2: float = other.num / other.denom
+
+        if f1 >= f2:
+            return True
+        else:
+            return False
+
+
+def get_number(prompt: str) -> float:
+    """ read and return a number from the user.
+        Loop until the user provides a valid number.
+    """
+    while True:
+        inp: str = input(prompt)
+        try:
+            return float(inp)
+        except ValueError:
+            print(f"Error: '{inp}' is not a number. Please try again...")
+
+
+def get_fraction(count: int) -> Fraction:
+    """ Ask the user for a numerator and denominator and return a valid Fraction """
+    while True:
+        num = get_number(f"Enter a numerator for Fraction {count}: ")
+
+        while True:
+            denom = get_number(f"Enter a denominator for Fraction {count}: ")
+            if denom != 0:
+                break
+            else:
+                print("Denominator cannot be 0. Please enter another number.")
+
+        fraction: Fraction = Fraction(num, denom)
+
+        return fraction
+
+def compute(f1: Fraction, operator: str, f2: Fraction) -> None:
+    """ Given two fractions and an operator, return the result
+        of applying the operator to the two fractions
+    """
+    result: Optional[Union[Fraction, bool]]  # just define the type of result, don't set a value
+    okay: bool = True # a variable to decide printing the result
+
+    if operator == '+':
+        result = f1 + f2
+    elif operator == "-":
+        result = f1 - f2
+    elif operator == "*":
+        result = f1 * f2
+    elif operator == "/":
+        result = f1 / f2
+    elif operator == "==":
+        result = f1 == f2
+    elif operator == "!=":
+        result = f1 != f2
+    elif operator == "<":
+        result = f1 < f2
+    elif operator == "<=":
+        result = f1 <= f2
+    elif operator == ">":
+        result = f1 > f2
+    elif operator == ">=":
+        result = f1 >= f2
+    else:
+        print(f"Error: '{operator}' is an unrecognized operator")
+        okay = False # unrecognized operator --> do not print the result
+
+    if okay:
+        print(f"{f1} {operator} {f2} ---> {result}")
+
+
+def main() -> None:
+    """ Fraction calculations """
+    print('Welcome to the fraction calculator!')
+    f1: Fraction = get_fraction(1)
+    operator: str = input("Operation (+, -, *, /, ==, !=, <, <=, >, >=): ")
+    f2: Fraction = get_fraction(2)
+
+    try:
+        compute(f1, operator, f2)
+    except ZeroDivisionError:
+        print("Zero Division Error!")
+
+
+if __name__ == '__main__':
+    main()
---
+++ venv/app_test.py
@@ -0,0 +1,167 @@
+""" Implement a test class for fractions to test
+    addition, subtraction, multiplication, division,
+    equal, not equal, less than, less than or equal to,
+    greater thangreater than or equal to
+
+    author: Fatih IZGI
+    date: 11-Feb-2020
+    version: python 3.6.9
+"""
+
+import unittest
+from HW03_Fatih_IZGI import Fraction
+
+
+class TestFraction(unittest.TestCase):
+    """ test class Fraction """
+
+    def test_init(self):
+        """ verify that the numerator and denominator are set properly """
+        f: Fraction = Fraction(3, 4)
+        self.assertEqual(f.num, 3)
+        self.assertEqual(f.denom, 4)
+
+    def test_init_exception(self):
+        """ verify that ZeroDivisionError is raised when appropriate """
+        with self.assertRaises(ZeroDivisionError):
+            Fraction(3, 0) # trying to create a Fraction with 0 denomiator -> ZeroDivisionError
+
+    def test_str(self):
+        """ verify that __str__ works properly """
+        f: Fraction = Fraction(1, 6)
+        self.assertEqual(str(f), "1/6")
+
+    def test_add(self):
+        """ verify Fraction addition """
+        f12: Fraction = Fraction(1, 2)
+        f34: Fraction = Fraction(3, 4)
+        self.assertEqual(f12 + f34, Fraction(10, 8))
+        self.assertEqual(f12, Fraction(1, 2))  # f12 should not have changed
+
+    def test_add_3_operands(self):
+        """ verify Fraction addition with more than two operands """
+        f12: Fraction = Fraction(1, 2)
+        f34: Fraction = Fraction(3, 4)
+        f44: Fraction = Fraction(4, 4)
+        self.assertTrue(f12 + f34 + f44 == Fraction(72, 32))
+
+    def test_sub(self):
+        """ verify Fraction substraction """
+        f44: Fraction = Fraction(4, 4)
+        f68: Fraction = Fraction(6, 8)
+        self.assertEqual(f44 - f68, Fraction(8, 32))
+        self.assertEqual(f44, Fraction(4, 4))  # f44 should not have changed
+
+    def test_sub_3_operands(self):
+        """ verify Fraction substraction with more than two operands """
+        f41: Fraction = Fraction(4, 1)
+        f55: Fraction = Fraction(5, 5)
+        f32: Fraction = Fraction(3, 2)
+        self.assertTrue(f41 - f55 - f32 == Fraction(15, 10))
+
+    def test_mul(self):
+        """ verify Fraction multiplication """
+        f128: Fraction = Fraction(12, 8)
+        f41: Fraction = Fraction(4, 1)
+        self.assertEqual(f128 * f41, Fraction(48, 8))
+        self.assertEqual(f128, Fraction(12, 8))  # f128 should not have changed
+
+    def test_mul_3_operands(self):
+        """ verify Fraction multiplication with more than two operands """
+        f52: Fraction = Fraction(5, 2)
+        f34: Fraction = Fraction(3, 4)
+        f68: Fraction = Fraction(6, 8)
+        self.assertTrue(f52 * f34 * f68 == Fraction(90, 64))
+
+    def test_truediv(self):
+        """ verify Fraction division """
+        f32: Fraction = Fraction(3, 2)
+        f912: Fraction = Fraction(9, 12)
+        self.assertEqual(f32 / f912, Fraction(36, 18))
+        self.assertEqual(f32, Fraction(3, 2))  # f32 should not have changed
+
+    def test_truediv_3_operands(self):
+        """ verify Fraction division with more than two operands """
+        f34: Fraction = Fraction(3, 4)
+        f55: Fraction = Fraction(5, 5)
+        f912: Fraction = Fraction(9, 12)
+        self.assertTrue(f34 / f55 / f912 == Fraction(180, 180))
+
+    def test_later_zerodivision(self):
+        """ verify ZeroDivisionError is raised when the second Fraction's result is 0 """
+        with self.assertRaises(ZeroDivisionError):
+            f34: Fraction = Fraction(3, 4)
+            f05: Fraction = Fraction(0, 5)  # nominator is 0, no error will be raised
+                                            # but this fraction will be the denominator of the first fraction
+            result = f34 / f05 # Then this division will raise a ZeroDivisionError
+
+    def test_eq(self):
+        """ verify Fraction equation (eq) """
+        f41: Fraction = Fraction(4, 1)
+        f34: Fraction = Fraction(3, 4)
+        f1510: Fraction = Fraction(15, 10)
+        f128: Fraction = Fraction(12, 8)
+
+        self.assertEqual(f41 == f34, False)
+        self.assertEqual(f1510 == f128, True)
+
+    def test_ne(self):
+        """ verify Fraction equation (ne) """
+        f12: Fraction = Fraction(1, 2)
+        f34: Fraction = Fraction(3, 4)
+        f48: Fraction = Fraction(4, 8)
+        f816: Fraction = Fraction(8, 16)
+
+        self.assertEqual(f12 != f34, True)
+        self.assertEqual(f48 != f816, False)
+
+    def test_lt(self):
+        """ verify Fraction less than """
+        f12: Fraction = Fraction(1, 2)
+        f34: Fraction = Fraction(3, 4)
+        f25: Fraction = Fraction(2, 5)
+        f24: Fraction = Fraction(2, 4)
+        fm13: Fraction = Fraction(-1, 3)
+        f1m3: Fraction = Fraction(1, -3)
+        f23: Fraction = Fraction(2, 3)
+
+        self.assertEqual(f34 < f12, False)
+        self.assertEqual(f25 < f24, True)
+        self.assertEqual(fm13 < f23, True) # test for negative numbers
+        self.assertEqual(f1m3 < f23, True)
+
+    def test_le(self):
+        """ verify Fraction less than or equal """
+        f12: Fraction = Fraction(1, 2)
+        f34: Fraction = Fraction(3, 4)
+        f25: Fraction = Fraction(2, 5)
+        f24: Fraction = Fraction(2, 4)
+
+        self.assertEqual(f25 <= f12, True)
+        self.assertEqual(f12 <= f12, True)
+        self.assertEqual(f34 <= f24, False)
+
+    def test_gt(self):
+        """ verify Fraction greater than """
+        f12: Fraction = Fraction(1, 2)
+        f34: Fraction = Fraction(3, 4)
+        f25: Fraction = Fraction(2, 5)
+        f24: Fraction = Fraction(2, 4)
+
+        self.assertEqual(f12 > f34, False)
+        self.assertEqual(f24 > f25, True)
+
+    def test_ge(self):
+        """ verify Fraction greater than or equal """
+        f12: Fraction = Fraction(1, 2)
+        f34: Fraction = Fraction(3, 4)
+        f25: Fraction = Fraction(2, 5)
+        f24: Fraction = Fraction(2, 4)
+
+        self.assertEqual(f25 >= f12, False)
+        self.assertEqual(f12 >= f12, True)
+        self.assertEqual(f34 >= f24, True)
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
